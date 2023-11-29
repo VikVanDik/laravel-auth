f952704009c67d951974af71cd1a42c937d978cc
VikVanDik <95vlc17@gmail.com>
2023-11-29 15:16:59 +0100

    first commit

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+<aside class="bg-dark">
+    <nav>
+        <ul>
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Lista post</a></li>
+
+        </ul>
+    </nav>
+</aside>
